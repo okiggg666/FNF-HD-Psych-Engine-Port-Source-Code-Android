780b50a56b84df8dc392192dda94bacde79d6f9d
Nuno-Filipe-Studios <109983796+Nuno-Filipe-Studios@users.noreply.github.com>
2022-11-26 17:11:21 +0100

    e

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -1,6 +1,8 @@
 package;
 
-#if (android && MODS_ALLOWED)
+
+
+#if android
 import android.Tools;
 import android.Permissions;
 import android.PermissionsList;
@@ -14,6 +16,7 @@ import haxe.CallStack;
 import haxe.io.Path;
 import sys.FileSystem;
 import sys.io.File;
+import flash.system.System;
 
 /**
  * ...
@@ -24,13 +27,13 @@ using StringTools;
 
 class SUtil
 {
-	#if (android && MODS_ALLOWED)
+	#if android
 	private static var aDir:String = null; // android dir
 	#end
 
 	public static function getPath():String
 	{
-		#if (android && MODS_ALLOWED)
+		#if android
 		if (aDir != null && aDir.length > 0)
 			return aDir;
 		else
@@ -42,35 +45,38 @@ class SUtil
 
 	public static function doTheCheck()
 	{
-		#if (android && MODS_ALLOWED)
+		#if android
 		if (!Permissions.getGrantedPermissions().contains(PermissionsList.READ_EXTERNAL_STORAGE) || !Permissions.getGrantedPermissions().contains(PermissionsList.WRITE_EXTERNAL_STORAGE))
 		{
 			Permissions.requestPermissions([PermissionsList.READ_EXTERNAL_STORAGE, PermissionsList.WRITE_EXTERNAL_STORAGE]);
-			SUtil.applicationAlert('Permissions', "if you accepted the permissions all good if not expect a crash" + '\n' + 'Press Ok to see what happens');//shitty way to stop the app
+			SUtil.applicationAlert('Permissions', "if you acceptd the permissions all good if not expect a crash" + '\n' + 'Press Ok to see what happens');
 		}
 
 		if (Permissions.getGrantedPermissions().contains(PermissionsList.READ_EXTERNAL_STORAGE) || Permissions.getGrantedPermissions().contains(PermissionsList.WRITE_EXTERNAL_STORAGE))
 		{
-			if (!FileSystem.exists(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file') + '/'))
-				FileSystem.createDirectory(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file') + '/');
+			if (!FileSystem.exists(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file')))
+				FileSystem.createDirectory(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file'));
 
-			if (!FileSystem.exists(SUtil.getPath() + 'assets/') && !FileSystem.exists(SUtil.getPath() + 'mods/'))
+			if (!FileSystem.exists(SUtil.getPath() + 'assets') && !FileSystem.exists(SUtil.getPath() + 'mods'))
 			{
-				SUtil.applicationAlert('Error!', "Whoops, seems you didn't extract the files from the .APK!\nPlease watch the tutorial by pressing OK.");
-				openLinkAndClose();
+				SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the files from the .APK!\nPlease watch the tutorial by pressing OK.");
+				CoolUtil.browserLoad('https://youtu.be/zjvkTmdWvfU');
+				System.exit(0);
 			}
 			else
 			{
-				if (!FileSystem.exists(SUtil.getPath() + 'assets/'))
+				if (!FileSystem.exists(SUtil.getPath() + 'assets'))
 				{
 					SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the assets/assets folder from the .APK!\nPlease watch the tutorial by pressing OK.");
-					openLinkAndClose();
+					CoolUtil.browserLoad('https://youtu.be/zjvkTmdWvfU');
+					System.exit(0);
 				}
 
-				if (!FileSystem.exists(SUtil.getPath() + 'mods/'))
+				if (!FileSystem.exists(SUtil.getPath() + 'mods'))
 				{
 					SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the assets/mods folder from the .APK!\nPlease watch the tutorial by pressing OK.");
-					openLinkAndClose();
+					CoolUtil.browserLoad('https://youtu.be/zjvkTmdWvfU');
+					System.exit(0);
 				}
 			}
 		}
@@ -82,15 +88,15 @@ class SUtil
 		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
 	}
 
-	static function onCrash(e:UncaughtErrorEvent):Void
+	public static function onCrash(e:UncaughtErrorEvent):Void
 	{
-		var errMsg:String = "";
-		var path:String;
 		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
 		var dateNow:String = Date.now().toString();
+		dateNow = StringTools.replace(dateNow, " ", "_");
+		dateNow = StringTools.replace(dateNow, ":", "'");
 
-		dateNow = dateNow.replace(" ", "_");
-		dateNow = dateNow.replace(":", "'");
+		var path:String = "crash/" + "crash_" + dateNow + ".txt";
+		var errMsg:String = "";
 
 		for (stackItem in callStack)
 		{
@@ -103,18 +109,19 @@ class SUtil
 			}
 		}
 
-		errMsg += "\nUncaught Error: " + e.error;
-		#if MODS_ALLOWED
-		if (!FileSystem.exists(SUtil.getPath() + "crash/"))
-			FileSystem.createDirectory(SUtil.getPath() + "crash/");
+		errMsg += e.error;
 
-		File.saveContent(path, errMsg + "\n");
-		#end
+		if (!FileSystem.exists(SUtil.getPath() + "crash"))
+		FileSystem.createDirectory(SUtil.getPath() + "crash");
+
+		File.saveContent(SUtil.getPath() + path, errMsg + "\n");
 
 		Sys.println(errMsg);
+		Sys.println("Crash dump saved in " + Path.normalize(path));
+		Sys.println("Making a simple alert ...");
 
-		Application.current.window.alert(errMsg, "Error!");
-		Sys.exit(1);
+		SUtil.applicationAlert("Uncaught Error :(!", errMsg);
+		System.exit(0);
 	}
 
 	private static function applicationAlert(title:String, description:String)
@@ -122,31 +129,20 @@ class SUtil
 		Application.current.window.alert(description, title);
 	}
 
-	private static function openLinkAndClose()
-	{
-		CoolUtil.browserLoad('https://youtu.be/zjvkTmdWvfU');
-		Sys.exit(1);
-	}
-
 	#if android
 	public static function saveContent(fileName:String = 'file', fileExtension:String = '.json', fileData:String = 'you forgot something to add in your code')
 	{
-		#if (android && MODS_ALLOWED)
-		if (!FileSystem.exists(SUtil.getPath() + "saves")){
-			FileSystem.createDirectory(SUtil.getPath() + "saves");
-		}
-		File.saveContent(SUtil.getPath() + "saves/" + fileName + fileExtension, fileData);
-		SUtil.applicationAlert("Done Action :)", "File Saved Successfully!");
-		#elseif android
-		openfl.system.System.setClipboard(fileData);
-		SUtil.applicationAlert("Done Action :)", "Data Saved to Clipboard Successfully!");
-		#end
+		if (!FileSystem.exists(SUtil.getPath() + 'saves'))
+			FileSystem.createDirectory(SUtil.getPath() + 'saves');
+
+		File.saveContent(SUtil.getPath() + 'saves/' + fileName + fileExtension, fileData);
+		SUtil.applicationAlert('Done :)!', 'File Saved Successfully!');
 	}
 
 	public static function saveClipboard(fileData:String = 'you forgot something to add in your code')
 	{
 		openfl.system.System.setClipboard(fileData);
-		SUtil.applicationAlert('Done!', 'Data Saved to Clipboard Successfully!');
+		SUtil.applicationAlert('Done :)!', 'Data Saved to Clipboard Successfully!');
 	}
 
 	public static function copyContent(copyPath:String, savePath:String)
